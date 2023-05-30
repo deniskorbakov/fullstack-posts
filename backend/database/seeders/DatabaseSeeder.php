623f307bbd2969de45b4679b0228acf6c1976eff
deniskorbakov <korbakovd@gmail.com>
2023-05-31 00:00:18 +0300

    add factory for new tables

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -3,7 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
+use App\Models\Comment;
+use App\Models\Like;
 use App\Models\Post;
+use App\Models\PostCategory;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -16,5 +20,9 @@ class DatabaseSeeder extends Seeder
     {
         User::factory(5)->create();
         Post::factory(30)->create();
+        Category::factory(10)->create();
+        Comment::factory(20)->create();
+        Like::factory(100)->create();
+        PostCategory::factory(60)->create();
     }
 }
