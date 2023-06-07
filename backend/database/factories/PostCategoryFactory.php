37bcb120baa4f316d9063cfc593ff7530487cfb4
deniskorbakov <korbakovd@gmail.com>
2023-06-07 21:27:24 +0300

    add fake data for postCategory

--- backend/database/factories/PostCategoryFactory.php
+++ backend/database/factories/PostCategoryFactory.php
@@ -17,7 +17,7 @@ class PostCategoryFactory extends Factory
     public function definition(): array
     {
         return [
-            'user_id' => rand(1, 5),
+            'category_id' => rand(1, 10),
             'post_id' => rand(1, 30),
         ];
     }
