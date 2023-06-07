bc3a89f214b7df94928d2de051de427f42c7f400
deniskorbakov <korbakovd@gmail.com>
2023-06-07 21:30:08 +0300

    add resource for like

---
+++ backend/app/Http/Resources/LikeResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class LikeResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'userName' => User::all()->where('id', $this->user_id)->value('name'),
+        ];
+    }
+}
