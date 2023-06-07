0399928c00aa284dacc5b7b92d360d2aa3a004b5
deniskorbakov <korbakovd@gmail.com>
2023-06-07 21:26:56 +0300

    change user_id on category_id

--- backend/database/migrations/2023_05_30_204931_create_post_categories_table.php
+++ backend/database/migrations/2023_05_30_204931_create_post_categories_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('post_categories', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained();
+            $table->foreignId('category_id')->constrained();
             $table->foreignId('post_id')->constrained();
             $table->timestamps();
         });
