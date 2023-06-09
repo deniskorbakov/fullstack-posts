84cb604ee9532e7de4a64d2ab9bcd96bc02a5374
deniskorbakov <korbakovd@gmail.com>
2023-06-10 02:04:41 +0300

    add validate on categories

--- backend/app/Http/Requests/PostRequest.php
+++ backend/app/Http/Requests/PostRequest.php
@@ -24,6 +24,7 @@ class PostRequest extends FormRequest
         return [
             'body' => 'required|string|min:10|max:255',
             'description' => 'required|string|min:5|max:50',
+            'categories' => 'array',
         ];
     }
 }
