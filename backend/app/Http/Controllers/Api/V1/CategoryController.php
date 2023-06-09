0b504d6fbdc671ed4b7ab44575269e83b05ce6db
deniskorbakov <korbakovd@gmail.com>
2023-06-10 02:03:36 +0300

    delete extra methods

--- backend/app/Http/Controllers/Api/V1/CategoryController.php
+++ backend/app/Http/Controllers/Api/V1/CategoryController.php
@@ -2,55 +2,17 @@
 
 namespace App\Http\Controllers\Api\V1;
 
-use App\Http\Requests\CategoryRequest;
 use App\Http\Resources\CategoryResource;
 use App\Models\Category;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class CategoryController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * @return AnonymousResourceCollection
      */
-    public function index()
+    public function all(): AnonymousResourceCollection
     {
-        return CategoryResource::collection(Category::all());
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(CategoryRequest $request)
-    {
-        $category = Category::create($request->validated());
-
-        return new CategoryResource($category);
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Category $category)
-    {
-        return new CategoryResource(Category::findOrFail($category->id));
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(CategoryRequest $request, Category $category)
-    {
-        $category->update($request->validated());
-
-        return new CategoryResource($category);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Category $category)
-    {
-        $category->delete();
-
-        return response(null, \Illuminate\Http\Response::HTTP_NO_CONTENT);
-    }
+       return CategoryResource::collection(Category::all());
+   }
 }
