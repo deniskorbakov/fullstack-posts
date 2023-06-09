8c8a53a3aa6307765251862e030f3938b2fb97de
deniskorbakov <korbakovd@gmail.com>
2023-06-10 02:06:57 +0300

    delete extra class

--- backend/app/Http/Resources/PostResource.php
+++ backend/app/Http/Resources/PostResource.php
@@ -6,7 +6,6 @@ use App\Models\Category;
 use App\Models\Comment;
 use App\Models\Like;
 use App\Models\Post;
-use App\Models\PostCategory;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
