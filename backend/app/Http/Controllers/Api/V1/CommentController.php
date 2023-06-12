b40335ac0e28415e1b60d7a4f28f4f9881861fd4
deniskorbakov <korbakovd@gmail.com>
2023-06-12 23:40:46 +0300

    add for comments methods update and delete

--- backend/app/Http/Controllers/Api/V1/CommentController.php
+++ backend/app/Http/Controllers/Api/V1/CommentController.php
@@ -5,14 +5,16 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Requests\CommentRequest;
 use App\Http\Resources\CommentResource;
+use App\Http\Resources\CommentResponseResource;
 use App\Models\Comment;
 use App\Models\Post;
+use Symfony\Component\HttpFoundation\Response as ResponseAlias;
 
 class CommentController extends Controller
 {
 
 
-    public function add(CommentRequest $request, Post $post) {
+    public function store(CommentRequest $request, Post $post) {
         $fields = $request->validated();
 
         $comment = Comment::create([
@@ -22,5 +24,33 @@ class CommentController extends Controller
         ]);
 
         return new CommentResource($comment);
+    }
+
+    public function update(Post $post, Comment $comment, CommentRequest $request) {
+        if (auth()->user()->getAuthIdentifier() == $comment->user_id && $post->id == $comment->post_id) {
+            $fields = $request->validated();
+
+            $comment->update([
+                'text' => $fields['text'],
+            ]);
+
+            return new CommentResource($comment);
+        } else {
+            return response([
+                'message' => 'you cannot update this comment',
+            ], 403);
+        }
+    }
+
+    public function destroy(Post $post, Comment $comment) {
+        if (auth()->user()->getAuthIdentifier() == $comment->user_id && $post->id == $comment->post_id) {
+            $comment->delete();
+
+            return response(null, ResponseAlias::HTTP_NO_CONTENT);
+        } else {
+            return response([
+                'message' => 'you cannot delete this comment',
+            ], 403);
+        }
     }
 }
