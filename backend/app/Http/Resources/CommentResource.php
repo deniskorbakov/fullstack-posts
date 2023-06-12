4c38534187b3f38f7d161fa6822fc8635fa1187f
deniskorbakov <korbakovd@gmail.com>
2023-06-12 18:28:04 +0300

    add column id in comments

--- backend/app/Http/Resources/CommentResource.php
+++ backend/app/Http/Resources/CommentResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\CommentResponse;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
@@ -16,8 +17,10 @@ class CommentResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'userName' => User::all()->where('id', $this->user_id)->value('name'),
-            'comment' => $this->text
+            'comment' => $this->text,
+            'responses' => CommentResponseResource::collection(CommentResponse::all()->where('comment_id', $this->id)),
         ];
     }
 }
