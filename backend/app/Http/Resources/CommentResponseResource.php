e8632c2b561ae52b3b3c567bdff821d291369a71
deniskorbakov <korbakovd@gmail.com>
2023-06-12 18:31:10 +0300

    create resource for responses

---
+++ backend/app/Http/Resources/CommentResponseResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CommentResponseResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'text' => $this->text,
+            'userName' => User::all()->where('id', $this->user_id)->value('name'),
+        ];
+    }
+}
