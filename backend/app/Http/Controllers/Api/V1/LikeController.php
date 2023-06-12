bd9b4e1dc939b3b8b89e0c2a30660f6b778b9965
deniskorbakov <korbakovd@gmail.com>
2023-06-12 23:41:10 +0300

    add for likes method delete

--- backend/app/Http/Controllers/Api/V1/LikeController.php
+++ backend/app/Http/Controllers/Api/V1/LikeController.php
@@ -6,10 +6,11 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Resources\LikeResource;
 use App\Models\Like;
 use App\Models\Post;
+use Symfony\Component\HttpFoundation\Response as ResponseAlias;
 
 class LikeController extends Controller
 {
-    public function add(Post $post) {
+    public function stor(Post $post) {
 
         if(Like::all()->where('user_id', auth()->user()->getAuthIdentifier())->where('post_id', $post->id)->count() > 0) {
             return response(['message' => 'do you not add more than two'], 403);
@@ -21,6 +22,19 @@ class LikeController extends Controller
             ]);
 
             return new LikeResource($like);
+        }
+    }
+
+    public function destroy(Post $post, Like $like) {
+        if (auth()->user()->getAuthIdentifier() == $like->user_id && $post->id == $like->post_id) {
+            $like->delete();
+
+            return response(null, ResponseAlias::HTTP_NO_CONTENT);
+        }
+        else {
+            return response([
+                'message' => 'you cannot delete this like',
+            ], 403);
         }
     }
 }
