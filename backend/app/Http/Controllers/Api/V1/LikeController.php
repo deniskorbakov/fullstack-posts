6a0edc4f1d48610d62c32a779065b795a249710d
deniskorbakov <korbakovd@gmail.com>
2023-06-17 23:51:50 +0300

    delete extra code in controller

--- backend/app/Http/Controllers/Api/V1/LikeController.php
+++ backend/app/Http/Controllers/Api/V1/LikeController.php
@@ -6,28 +6,20 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Resources\LikeResource;
 use App\Models\Like;
 use App\Models\Post;
+use App\Services\LikeService;
 use Symfony\Component\HttpFoundation\Response as ResponseAlias;
 
 class LikeController extends Controller
 {
-    public function store(Post $post) {
+    public function store(Post $post, LikeService $service) {
+        $like = $service->store($post);
 
-        if(Like::all()->where('user_id', auth()->user()->getAuthIdentifier())->where('post_id', $post->id)->count() > 0) {
-            return response(['message' => 'do you not add more than two'], 403);
-        }
-        else {
-            $like = Like::create([
-                'user_id' => auth()->user()->getAuthIdentifier(),
-                'post_id' => $post->id,
-            ]);
-
-            return new LikeResource($like);
-        }
+        return new LikeResource($like);
     }
 
     public function destroy(Post $post, Like $like) {
-            $like->delete();
+        $like->delete();
 
-            return response(null, ResponseAlias::HTTP_NO_CONTENT);
+        return response(null, ResponseAlias::HTTP_NO_CONTENT);
     }
 }
