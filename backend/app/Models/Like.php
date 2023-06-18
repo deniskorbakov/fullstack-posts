8e01bd0b36fda56aa201c80cc52bda83a38ba952
deniskorbakov <korbakovd@gmail.com>
2023-06-18 14:28:39 +0300

    create method for users

--- backend/app/Models/Like.php
+++ backend/app/Models/Like.php
@@ -11,4 +11,8 @@ class Like extends Model
     use HasFactory, SoftDeletes;
 
     protected $guarded = ['id'];
+
+    public function users() {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
 }
