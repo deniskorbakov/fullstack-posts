5d9fa853d245b082d0292ed1e7d2639596fa6677
deniskorbakov <korbakovd@gmail.com>
2023-06-26 23:26:02 +0300

    add title bar component

---
+++ frontend/src/components/Main/TitleBar/TitleBar.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <button class="flex space-x-4 px-4 py-2 rounded-full w-full">
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped>
+
+</style>
