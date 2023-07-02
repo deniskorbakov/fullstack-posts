8280af86033b89b649570d07f9454ce994a20b1b
deniskorbakov <korbakovd@gmail.com>
2023-07-02 18:27:27 +0300

    add components for posts

---
+++ frontend/src/components/Main/PostCreate/PostCreate.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+import TipTap from "../../TipTape/TipTap.vue";
+</script>
+
+<template>
+  <div class="bg-white rounded-lg mt-10 p-5">
+    <input class="mb-5 text-4xl" placeholder="Введите название статьи" style="outline: none">
+    <TipTap></TipTap>
+  </div>
+</template>
+
+<style>
+
+</style>
