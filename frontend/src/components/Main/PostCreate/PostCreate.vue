15460bbd849c5967a28d9285258711945c386e51
deniskorbakov <korbakovd@gmail.com>
2023-07-03 20:17:20 +0300

    add new styles and input title

--- frontend/src/components/Main/PostCreate/PostCreate.vue
+++ frontend/src/components/Main/PostCreate/PostCreate.vue
@@ -4,8 +4,7 @@ import TipTap from "../../TipTape/TipTap.vue";
 </script>
 
 <template>
-  <div class="bg-white rounded-lg mt-10 p-5">
-    <input class="mb-5 text-4xl" placeholder="Введите название статьи" style="outline: none">
+  <div class="bg-white rounded-lg mt-10 p-5 max-w-5xl">
     <TipTap></TipTap>
   </div>
 </template>
