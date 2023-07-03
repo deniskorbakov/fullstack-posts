9801da8ecfce91f352c841ef9d9aa1a3f5dbf184
deniskorbakov <korbakovd@gmail.com>
2023-07-03 20:16:35 +0300

    add component for create comment

---
+++ frontend/src/components/Main/Post/CommentCreate/CommentCreate.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="container bg-white h-max p-10 rounded mt-10">
+    <div class="flex">
+      <div class="flex-initial w-64">
+        <p>47 Коментариев</p>
+      </div>
+
+      <div class="flex-initial w-full text-right">
+        <p class="">фильтрация</p>
+      </div>
+    </div>
+
+    <div class="mt-5">
+      <textarea class="bg-gray-900 text-white px-4 py-4 rounded-lg w-full h-32 resize-y outline-none" placeholder="Написать комментарий"></textarea>
+    </div>
+
+    <div class="mt-5 text-right">
+      <button class="bg-cyan-500 rounded-lg px-4 py-2 hover:bg-cyan-600">Создать Коммент</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
