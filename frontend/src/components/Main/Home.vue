8134900816e3bc15f2de2a2fe01e2f226d7464f2
deniskorbakov <korbakovd@gmail.com>
2023-07-06 02:24:37 +0300

    create component for main page

---
+++ frontend/src/components/Main/Home.vue
@@ -0,0 +1,23 @@
+<script setup>
+  import PostItem from "./Post/PostItem/PostItem.vue";
+  import {usePostListStore} from "../../stores/postListStore.js";
+  import {storeToRefs} from "pinia";
+  import {onMounted} from "vue";
+
+  const store = usePostListStore()
+
+  const {getPosts} = store
+  const {posts} = storeToRefs(store)
+
+ onMounted(getPosts)
+</script>
+
+<template>
+  <div>
+   <PostItem v-for="item in posts.value" :data="item"></PostItem>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
