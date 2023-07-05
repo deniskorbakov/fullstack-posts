7b89fa33792a2d70dc80ce8063306aa09b71d392
deniskorbakov <korbakovd@gmail.com>
2023-07-06 02:24:58 +0300

    add new path and pinia

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,44 +2,28 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import {createRouter, createWebHistory} from "vue-router";
-import Content from "./components/Main/PostItem/PostItem.vue";
 import Registration from "./components/Main/Registration/Registration.vue";
-import Auth from "./components/Main/Auth/Auth.vue";
-import Post from "./components/Main/Post/Post.vue";
-import PostCreate from "./components/Main/PostCreate/PostCreate.vue";
+import Auth from "./components/Main/Auth/Auth.vue";;
+import PostCreate from "./components/Main/Post/PostCreate/PostCreate.vue";
+import {createPinia} from "pinia";
+import Home from "./components/Main/Home.vue";
+import PostId from "./components/Main/Post/PostId.vue";
+
+const routes = [
+    { path: '/', component: Home },
+    { path: '/registration',  component: Registration },
+    { path: '/auth', component: Auth },
+    { path: '/create', component: PostCreate },
+    { path: '/post/:id', name: 'postId', component: PostId },
+]
 
 const router = createRouter({
-    routes: [
-        {
-            path: '/',
-            component: Content
-        },
-
-        {
-            path: '/registration',
-            component: Registration
-        },
-
-        {
-            path: '/auth',
-            component: Auth
-        },
-
-        {
-            path: '/post',
-            component: Post
-        },
-
-        {
-            path: '/create',
-            component: PostCreate
-        },
-    ],
-
-    history: createWebHistory()
+    history: createWebHistory(),
+    routes,
 })
 
 const app = createApp(App)
+app.use(createPinia())
 app.use(router)
 app.mount('#app')
 
