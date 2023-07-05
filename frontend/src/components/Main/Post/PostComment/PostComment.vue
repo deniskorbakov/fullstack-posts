7cf61ba2be4045e2aeb611f8a8bf11b8501c8325
deniskorbakov <korbakovd@gmail.com>
2023-07-06 02:25:28 +0300

    add props

--- frontend/src/components/Main/Post/PostComment/PostComment.vue
+++ frontend/src/components/Main/Post/PostComment/PostComment.vue
@@ -1,5 +1,11 @@
 <script setup>
-
+  const props = defineProps({
+    data: {
+      type: Object,
+      required: true,
+      default: () => {}
+    }
+  })
 </script>
 
 <template>
@@ -7,14 +13,14 @@
     <div class="overflow-hidden relative max-w-xs shadow-lg ring-1 ring-black/5 rounded-xl flex items-center gap-6 bg-gray-900">
       <img class="absolute -left-6 w-24 h-24 rounded-full shadow-lg" src="https://images.unsplash.com/photo-1501196354995-cbb51c65aaea?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=facearea&amp;facepad=4&amp;w=256&amp;h=256&amp;q=80">
       <div class="flex flex-col py-5 pl-24 text-white">
-        <strong class="text-sm font-medium">Denis korbakov</strong>
+        <strong class="text-sm font-medium">{{props.data['userName']}}</strong>
         <span class="text-sm font-medium text-gray-500">Вчера</span>
       </div>
     </div>
 
     <div class="mt-5 ml-5 p-2 h-max ">
       <p class="break-words">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci autem consectetur cupiditate distinctio, dolor earum facilis illum inventore, ipsum laudantium magnam nemo officiis perspiciatis porro reprehenderit rerum suscipit tempora unde.
+        {{props.data['comment']}}
       </p>
     </div>
 
