562495ecae205c321c2d8c19f6ba91219d3c252d
deniskorbakov <korbakovd@gmail.com>
2023-07-08 22:39:08 +0300

    update routes

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,18 +3,51 @@ import './style.css'
 import App from './App.vue'
 import {createRouter, createWebHistory} from "vue-router";
 import Registration from "./components/Main/Registration/Registration.vue";
-import Auth from "./components/Main/Auth/Auth.vue";;
+import Auth from "./components/Main/Auth/Auth.vue";
 import PostCreate from "./components/Main/Post/PostCreate/PostCreate.vue";
 import {createPinia} from "pinia";
 import Home from "./components/Main/Home.vue";
 import PostId from "./components/Main/Post/PostId.vue";
+import Me from "./components/Main/Me/Me.vue";
+
 
 const routes = [
-    { path: '/', component: Home },
-    { path: '/registration',  component: Registration },
-    { path: '/auth', component: Auth },
-    { path: '/create', component: PostCreate },
-    { path: '/post/:id', name: 'postId', component: PostId },
+    {
+        path: '/',
+        component: Home
+    },
+    {
+        path: '/registration',
+        name: 'reg',
+        component: Registration,
+    },
+    {
+        path: '/auth',
+        name: 'auth',
+        component: Auth,
+    },
+    {
+        path: '/create',
+        component: PostCreate
+    },
+    {
+        path: '/post/:id',
+        name: 'postId',
+        component: PostId },
+    {
+        path: '/me',
+        name: 'me',
+        component: Me,
+        meta: {requiresAuth: true},
+        beforeEnter: (from, to, next) => {
+            const userAuth = localStorage.getItem('token');
+
+            if (userAuth) {
+                next()
+            } else {
+                next('/auth')
+            }
+    }   }
 ]
 
 const router = createRouter({
