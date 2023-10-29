5cd9239a0e32ac57be4705c30882305aa828aee6
deniskorbakov <korbakovd@gmail.com>
2023-10-29 11:46:51 +0300

    feat: create popUp

---
+++ frontend/src/components/PopUp/PopUp.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="absolute top-[200px] right-[10px] bg-white px-10 py-5 border-4 border-cyan-500 rounded-lg">
+    <p class="text-2xl">
+      <slot/>
+    </p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
