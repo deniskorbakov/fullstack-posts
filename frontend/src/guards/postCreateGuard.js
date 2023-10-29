e00c9bb998cfda5411bad57c8746f9adc2f64c19
deniskorbakov <korbakovd@gmail.com>
2023-10-29 11:47:30 +0300

    feat: add method for popUp

--- frontend/src/guards/postCreateGuard.js
+++ frontend/src/guards/postCreateGuard.js
@@ -1,10 +1,16 @@
+import {usePopUpStore} from "../stores/popUpStore.js";
+
+const popUpStore = usePopUpStore();
+
+const {isShowPopUpWindow} = popUpStore;
+
 export const postCreateGuard = (to, from, next) => {
     const isAuthenticated = localStorage.getItem('token')
 
     if (to.name === 'create' && isAuthenticated) {
         next();
     } else if (to.name === 'create' && !isAuthenticated) {
-        next({name: 'home'});
+        isShowPopUpWindow('Вы не зарегистрированы');
     } else if (to.name !== 'create' && !isAuthenticated) {
         next();
     } else {
