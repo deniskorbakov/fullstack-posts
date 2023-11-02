f8fa6cf39c192407cea23ec4e09d2c52c012a369
deniskorbakov <korbakovd@gmail.com>
2023-11-02 23:30:38 +0300

    feat: add new method

--- frontend/src/guards/meGuard.js
+++ frontend/src/guards/meGuard.js
@@ -1,10 +1,17 @@
+import {usePopUpStore} from "../stores/popUpStore.js";
+
 export const meGuard = (to, from, next) => {
     const isAuthenticated = localStorage.getItem('token')
+
+    const popUpStore = usePopUpStore();
+
+    const {isShowPopUpWindow} = popUpStore;
 
     if (to.name === 'me' && isAuthenticated) {
         next();
     } else if (to.name === 'me' && !isAuthenticated) {
         next({name: 'auth'});
+        isShowPopUpWindow('Вы не авторизованы');
     } else if (to.name !== 'me' && !isAuthenticated) {
         next();
     } else {
--- frontend/src/guards/postCreateGuard.js
+++ frontend/src/guards/postCreateGuard.js
@@ -1,16 +1,17 @@
 import {usePopUpStore} from "../stores/popUpStore.js";
 
-const popUpStore = usePopUpStore();
-
-const {isShowPopUpWindow} = popUpStore;
-
 export const postCreateGuard = (to, from, next) => {
+    const popUpStore = usePopUpStore();
+
+    const {isShowPopUpWindow} = popUpStore;
+
     const isAuthenticated = localStorage.getItem('token')
 
     if (to.name === 'create' && isAuthenticated) {
         next();
     } else if (to.name === 'create' && !isAuthenticated) {
-        isShowPopUpWindow('Вы не зарегистрированы');
+        next({name: 'home'});
+        isShowPopUpWindow('Вы не авторизованы');
     } else if (to.name !== 'create' && !isAuthenticated) {
         next();
     } else {
