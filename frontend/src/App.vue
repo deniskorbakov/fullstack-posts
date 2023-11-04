d98e9cfd43b7b7c58a90a152afd26796d61ad2c6
deniskorbakov <korbakovd@gmail.com>
2023-11-04 12:14:17 +0300

    fix: delete extra code

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,7 +3,6 @@ import Header from "./components/Header/Header.vue";
 import SideBar from "./components/Main/SideBar/SideBar.vue";
 import {useSidebarStore} from "./stores/sidebarStore.js";
 import {storeToRefs} from "pinia";
-import {onMounted} from "vue";
 import {useAuthStore} from "./stores/authStore.js";
 import PopUp from "./components/PopUp/PopUp.vue";
 import {usePopUpStore} from "./stores/popUpStore.js";
@@ -16,15 +15,11 @@ const {show} = storeToRefs(store);
 
 const storeAuth = useAuthStore();
 
-const {isAuth} = storeAuth;
 const {user} = storeToRefs(storeAuth);
 
 const popUpStore = usePopUpStore();
 
 const {isShow, message} = storeToRefs(popUpStore);
-
-
-onMounted(isAuth)
 </script>
 
 
@@ -32,9 +27,7 @@ onMounted(isAuth)
   <div id="app">
     <Header :show="show" :toggle="getSidebar" :user="user"></Header>
     <div id="main" class="flex bg-gray-800 text-white h-screen">
-      <Transition>
         <SideBar v-show="show"></SideBar>
-      </Transition>
       <div class="bg-gray-900 w-full text-black example">
         <div class="w-1/2 h-screen mx-auto overflow-scroll scrollbar-hide">
           <RouterView/>
