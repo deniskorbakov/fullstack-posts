9e717a0958555e5f9393fade7683641da57702dd
deniskorbakov <korbakovd@gmail.com>
2023-11-09 08:09:52 +0300

    fix: update eloquent for methods

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -2,8 +2,8 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -17,6 +17,14 @@ class User extends Authenticatable
     public function posts(): HasMany
     {
         return $this->hasMany(Post::class);
+    }
+
+    public function subscribers(): BelongsToMany {
+        return $this->belongsToMany(Subscriber::class);
+    }
+
+    public function subscriptions(): BelongsToMany {
+        return $this->belongsToMany(Subscription::class);
     }
 
 }
