32914c9b159e990c4c89bda40eade78090b292c2
deniskorbakov <korbakovd@gmail.com>
2023-11-09 08:10:30 +0300

    feat: creates models

---
+++ backend/app/Models/Subscriber.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Subscriber extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    public function users(): BelongsToMany {
+        return $this->belongsToMany(User::class);
+    }
+}
