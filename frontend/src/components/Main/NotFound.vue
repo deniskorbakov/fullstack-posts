98d4f796b2bc10a835d4e6156a8d56559a60c4b6
deniskorbakov <korbakovd@gmail.com>
2023-11-12 18:00:56 +0300

    feat: create 404 page

---
+++ frontend/src/components/Main/NotFound.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="h-screen flex flex-grow items-center justify-center">
+    <div class="rounded-lg bg-white p-8 text-center shadow-xl">
+      <h1 class="mb-4 text-4xl font-bold">404</h1>
+      <p class="text-gray-600">Oops! The page you are looking for could not be found.</p>
+      <a href="/" class="mt-4 inline-block rounded bg-sky-800 px-4 py-2 font-semibold text-white"> Go back to Home </a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
