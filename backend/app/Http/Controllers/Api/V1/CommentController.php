147158218ba3002bdd2e64e8181260b2f3e21a2c
deniskorbakov <korbakovd@gmail.com>
2023-11-25 13:18:37 +0300

    feat: add actions

--- backend/app/Http/Controllers/Api/V1/CommentController.php
+++ backend/app/Http/Controllers/Api/V1/CommentController.php
@@ -2,43 +2,32 @@
 
 namespace App\Http\Controllers\Api\V1;
 
-
+use App\Actions\Comments\CommentDestroy;
+use App\Actions\Comments\CommentStore;
+use App\Actions\Comments\CommentUpdate;
 use App\Http\Requests\CommentRequest;
 use App\Http\Resources\CommentResource;
-use App\Http\Resources\CommentResponseResource;
 use App\Models\Comment;
 use App\Models\Post;
-use Symfony\Component\HttpFoundation\Response as ResponseAlias;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\Routing\ResponseFactory;
+use Illuminate\Foundation\Application as ApplicationAlias;
+use Illuminate\Http\Response;
 
 class CommentController extends Controller
 {
-
-
-    public function store(CommentRequest $request, Post $post) {
-        $fields = $request->validated();
-
-        $comment = Comment::create([
-            'post_id' => $post->id,
-            'user_id' => auth()->user()->getAuthIdentifier(),
-            'text' => $fields['text'],
-        ]);
-
-        return new CommentResource($comment);
+    public function store(CommentRequest $request, Post $post, CommentStore $action): CommentResource
+    {
+        return $action($request, $post);
     }
 
-    public function update(Post $post, Comment $comment, CommentRequest $request) {
-        $fields = $request->validated();
-
-        $comment->update([
-            'text' => $fields['text'],
-        ]);
-
-        return new CommentResource($comment);
+    public function update(Comment $comment, CommentRequest $request, CommentUpdate $action): CommentResource
+    {
+        return  $action($comment, $request);
     }
 
-    public function destroy(Post $post, Comment $comment) {
-        $comment->delete();
-
-        return response(null, ResponseAlias::HTTP_NO_CONTENT);
+    public function destroy(Comment $comment, CommentDestroy $action): ApplicationAlias|Response|Application|ResponseFactory
+    {
+        return $action($comment);
     }
 }
