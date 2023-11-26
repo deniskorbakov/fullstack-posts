87106d90bd4d674c513d8385917318b515083efd
deniskorbakov <korbakovd@gmail.com>
2023-11-26 20:00:06 +0300

    feat: add logic for verify this post

--- backend/app/Actions/Comments/CommentDestroy.php
+++ backend/app/Actions/Comments/CommentDestroy.php
@@ -4,6 +4,7 @@ namespace App\Actions\Comments;
 
 use App\Contracts\Comments\CommentDestroyContract;
 use App\Models\Comment;
+use App\Models\Post;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Foundation\Application as ApplicationAlias;
@@ -12,8 +13,12 @@ use Symfony\Component\HttpFoundation\Response as ResponseAlias;
 
 class CommentDestroy implements CommentDestroyContract
 {
-    public function __invoke(Comment $comment): ApplicationAlias|Response|Application|ResponseFactory
+    public function __invoke(Comment $comment, Post $post): ApplicationAlias|Response|Application|ResponseFactory
     {
+        if($comment->post_id !== $post->id) {
+            return response(['message' => 'Нету данного коментария у данного поста'], 403);
+        }
+
         $comment->delete();
 
         return response(null, ResponseAlias::HTTP_NO_CONTENT);
--- backend/app/Actions/Comments/CommentUpdate.php
+++ backend/app/Actions/Comments/CommentUpdate.php
@@ -6,15 +6,23 @@ use App\Contracts\Comments\CommentUpdateContract;
 use App\Http\Requests\CommentRequest;
 use App\Http\Resources\CommentResource;
 use App\Models\Comment;
+use App\Models\Post;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\Routing\ResponseFactory;
+use Illuminate\Foundation\Application as ApplicationAlias;
+use Illuminate\Http\Response;
 
 class CommentUpdate implements CommentUpdateContract
 {
-    public function __invoke(Comment $comment, CommentRequest $request): CommentResource
+    public function __invoke(Comment $comment, array|CommentRequest $request, Post $post): CommentResource|ApplicationAlias|Response|Application|ResponseFactory
     {
-        $fields = $request->validated();
+
+        if($comment->post_id !== $post->id) {
+            return response(['message' => 'Нету данного коментария у данного поста'], 403);
+        }
 
         $comment->update([
-            'text' => $fields['text'],
+            'text' => $request['text'],
         ]);
 
         return new CommentResource($comment);
