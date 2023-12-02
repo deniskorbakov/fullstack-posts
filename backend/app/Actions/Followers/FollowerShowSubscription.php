a822f30dc2b1be9b5438ed3a0c09a55e02b8d4e3
deniskorbakov <korbakovd@gmail.com>
2023-12-02 17:27:36 +0300

    fix: update logic action

--- backend/app/Actions/Followers/FollowerShowSubscription.php
+++ backend/app/Actions/Followers/FollowerShowSubscription.php
@@ -3,13 +3,15 @@
 namespace App\Actions\Followers;
 
 use App\Contracts\Followers\FollowerShowSubscriptionContract;
+use App\Http\Resources\SubscriptionResource;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class FollowerShowSubscription implements FollowerShowSubscriptionContract
 {
-    public function __invoke(User $user): Collection
+    public function __invoke(User $user): AnonymousResourceCollection
     {
-        return $user->following;
+        return SubscriptionResource::collection($user->following);
     }
 }
