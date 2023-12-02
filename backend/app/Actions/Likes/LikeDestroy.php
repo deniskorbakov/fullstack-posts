8e0d866278467f2f90b8bc954ae5af3709dbecb8
deniskorbakov <korbakovd@gmail.com>
2023-12-02 15:55:31 +0300

    feat: create actions for like controller

--- backend/app/Actions/Likes/LikeDestroy.php
+++ backend/app/Actions/Likes/LikeDestroy.php
@@ -2,7 +2,17 @@
 
 namespace App\Actions\Likes;
 
-class LikeDestroy
-{
+use App\Contracts\Likes\LikeDestroyContract;
+use App\Models\Like;
+use Illuminate\Http\JsonResponse;
+use Symfony\Component\HttpFoundation\Response as ResponseAlias;
 
+class LikeDestroy implements LikeDestroyContract
+{
+    public function __invoke(Like $like): JsonResponse
+    {
+        $like->delete();
+
+        return response()->json(null, ResponseAlias::HTTP_NO_CONTENT);
+    }
 }
