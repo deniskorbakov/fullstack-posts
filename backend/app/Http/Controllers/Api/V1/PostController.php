335fde8c8605c80a8e2596de1ca411f21750f538
deniskorbakov <korbakovd@gmail.com>
2023-12-02 16:33:46 +0300

    fix: update logic for post controller

--- backend/app/Http/Controllers/Api/V1/PostController.php
+++ backend/app/Http/Controllers/Api/V1/PostController.php
@@ -2,52 +2,42 @@
 
 namespace App\Http\Controllers\Api\V1;
 
+use App\Actions\Posts\PostDestroy;
+use App\Actions\Posts\PostIndex;
+use App\Actions\Posts\PostShow;
+use App\Actions\Posts\PostStore;
+use App\Actions\Posts\PostUpdate;
 use App\Http\Requests\PostRequest;
 use App\Http\Resources\PostResource;
 use App\Models\Post;
-use App\Services\PostService;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response as ResponseAlias;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class PostController extends Controller
 {
-    public function index(Request $request)
+    public function index(Request $request, PostIndex $action): AnonymousResourceCollection
     {
-        $page = ($request->page ?? 1) * 10;
-
-        $skip = $page - 10;
-
-        return PostResource::collection(Post::orderBy('id', 'desc')->skip($skip)->paginate(10));
+        return $action($request);
     }
 
-    public function store(PostRequest $request, PostService $service)
+    public function store(PostRequest $request, PostStore $action): PostResource
     {
-        $fields = $request->validated();
-
-        $post = $service->store($fields);
-
-        return new PostResource($post);
+        return $action($request);
     }
 
-    public function show(Post $post)
+    public function show(Post $post, PostShow $action): PostResource
     {
-        return new PostResource(Post::findOrFail($post->id));
+        return $action($post);
     }
 
-    public function update(PostRequest $request, Post $post, PostService $service)
+    public function update(PostRequest $request, Post $post, PostUpdate $action): PostResource
     {
-        $fields = $request->validated();
-
-        $post = $service->update($fields, $post);
-
-        return new PostResource($post);
+        return $action($request, $post);
     }
 
-    public function destroy(Post $post)
+    public function destroy(Post $post, PostDestroy $action): JsonResponse
     {
-        $post->delete();
-
-        return response(null, ResponseAlias::HTTP_NO_CONTENT);
+        return $action($post);
     }
-
 }
