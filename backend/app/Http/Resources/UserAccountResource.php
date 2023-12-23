44ab300a7789506501d04d71e8070a2aefc59848
deniskorbakov <korbakovd@gmail.com>
2023-12-23 23:51:03 +0300

    feat: add constructor

--- backend/app/Http/Resources/UserAccountResource.php
+++ backend/app/Http/Resources/UserAccountResource.php
@@ -2,11 +2,16 @@
 
 namespace App\Http\Resources;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class UserAccountResource extends JsonResource
 {
+    public function __construct(private User $user)
+    {
+        parent::__construct($user);
+    }
     /**
      * Transform the resource into an array.
      *
@@ -14,11 +19,13 @@ class UserAccountResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+
         return [
-            'name' => $this->name,
-            'email' => $this->email,
-            'posts' => $this->posts->count(),
-            'createdAccount' => date($this->created_at),
+            'name' => $this->user->name,
+            'email' => $this->user->email,
+            'posts' => $this->user->posts->count(),
+            'createdAccount' => date($this->user->created_at),
+            'loginInformation' => LoginInfoResource::collection($this->user->loginInfos()->get()),
         ];
     }
 }
